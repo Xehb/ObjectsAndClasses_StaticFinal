091a50cbca7b7ea96408e2238b8f080aebb32624
Xehb <xehb@list.ru>
2022-04-05 22:11:03 +0300

    finished practice 5.10.4

--- Encapsulation/04_Copy/src/ru/skillbox/Main.java
+++ Encapsulation/04_Copy/src/ru/skillbox/Main.java
@@ -6,7 +6,7 @@ public class Main {
      Dimensions cube = new Dimensions(4,1,-1);
         cube.calculateVolume();
 
-     CargoInformation cargo1 = new CargoInformation(5,"asf", true, "231123123", false, cube);
+     CargoInformation cargo1 = new CargoInformation(5,"asf", true, "XYN", false, cube);
      System.out.println("Номер груза:  " + cargo1.getRegistrationNumber()
              +"\n" + "Объем груза:  " + cargo1.getDimensions().calculateVolume() + " (метров в кубе)"
              +"\n" + "Вес груза:  " + cargo1.getWeight() + " (кг)");
