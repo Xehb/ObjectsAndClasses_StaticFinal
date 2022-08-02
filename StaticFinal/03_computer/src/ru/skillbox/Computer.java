2937b9621d10c1e7af3ddcde41eda81442585a78
Xehb <xehb@list.ru>
2022-08-02 12:19:48 +0300

    задание 6.8/2

---
+++ StaticFinal/03_computer/src/ru/skillbox/Computer.java
@@ -0,0 +1,37 @@
+package ru.skillbox;
+
+public class Computer {
+
+
+
+    private int сpu;
+    private int ram;
+    private int informationStorage;
+    private int screen;
+    private int keyboard;
+
+    private String cpuName(String name) {
+
+    }
+
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public void setInformationStorage(int informationStorage) {
+        this.informationStorage = informationStorage;
+    }
+
+    public void setScreen(int screen) {
+        this.screen = screen;
+    }
+
+    public void setKeyboard(int keyboard) {
+        this.keyboard = keyboard;
+    }
+
+    public void setСpu(int сpu) {
+        this.сpu = сpu;
+    }
+}
